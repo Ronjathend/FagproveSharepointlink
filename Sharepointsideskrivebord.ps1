964e5fd3094e4e3839c8cd272a3a79ec86bbc73b
Ronjathend <101792920+Ronjathend@users.noreply.github.com>
2024-05-29 11:21:38 +0200

    Add files via upload

---
+++ Sharepointsideskrivebord.ps1
@@ -0,0 +1,10 @@
+﻿$SharePointURL = "https://ronjafagprove.sharepoint.com/"
+
+
+$ShortcutPath = "$env:Public\Desktop\SharePoint Snarvei.lnk"
+
+$WScriptShell = New-Object -ComObject WScript.Shell
+
+$Shortcut = $WScriptShell.CreateShortcut($ShortcutPath)
+$Shortcut.TargetPath = $SharePointURL
+$Shortcut.Save()
